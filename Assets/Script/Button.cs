9c2ad6459f5711ed785fb987fc713e0384570cf2
setyabudi Utomo <30994833+setyabudiutomo@users.noreply.github.com>
2018-03-06 15:15:19 +0700

    Add files via upload

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+	public string btnName;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown (btnName)) {
+			Debug.Log("alhamdulillah");
+		}
+		if (Input.GetButtonUp (btnName)) {
+			Debug.Log("OK");
+		}
+	}
+}
